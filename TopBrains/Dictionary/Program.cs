2d3ce69225b428b1c882f0e02645d64e6ba3e325
Ayush Dubey <ayushdu37@gmail.com>
2026-02-04 14:55:40 +0530

    Dictionary => TopBrains

---
+++ TopBrains/Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static int TotalSalary(Dictionary<int, int> employeeSalaries, List<int> ids)
+    {
+        int totalSalary = 0;
+
+        foreach(int id in ids)
+        {
+            if (employeeSalaries.ContainsKey(id))
+            {
+                totalSalary += employeeSalaries[id];
+            }
+        }
+        return totalSalary;
+    }
+    public static void Main()
+    {
+        Dictionary<int, int> salaries = new Dictionary<int, int>()
+        {
+            {1, 20000},
+            {4, 40000},
+            {5, 15000}
+        };
+
+        List<int> ids = new List<int>(){1,4,5};
+
+        int result = TotalSalary(salaries, ids);
+        Console.WriteLine(result);
+    }
+}
