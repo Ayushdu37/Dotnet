7d14c407a524acd2fd7245f264ad0bb232ece9f7
Ayush Dubey <ayushdu37@gmail.com>
2026-02-04 14:46:10 +0530

    Time Conversion => TopBrains

---
+++ TopBrains/TimeConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static string ConvertTime(int totalSeconds)
+    {
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        return minutes + ":" + seconds.ToString("D2");
+    }
+    public static void Main()
+    {
+        int totalSeconds = int.Parse(Console.ReadLine());
+        string result = ConvertTime(totalSeconds);
+        Console.WriteLine(result);
+    }
+}
